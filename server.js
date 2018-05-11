c42454b0964e43bbc04e8ad9df05d656a5126d07
104207314 <104207314@mail2.nccu.tw>
2018-05-11 04:16:11 -0400

    startup

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine','ejs');
-app.listen(3000, function() {
-  db.sequelize.sync();
-});
 
 var routes = require('./routes')(app);
+//===========Express Routes Here=======//
+app.get("/",function(req,res){
+  res.send("<h1>Hello World</h1>");
+})
+//catch all other routes
+app.get("*",function(req,res){
+  res.send("<h1>Invalid Pages</h1>")
+});
+
+//=====sever listening on port3000
+app.listen(3000, function() {
+  db.sequelize.sync();
+  console.log("sever started on port 3000");
+});
