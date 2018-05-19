cc317ca9a1575658d19796aa335aa8f53ace4764
104207314 <104207314@mail2.nccu.tw>
2018-05-19 05:58:25 -0400

    add

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,18 +1,39 @@
 var db = require('../models');
 var Task = db.Task;
+var User = db.User;
 
 let taskController = {
   index: function (req, res) {
     Task.findAll()
-      .then(function (tasks) {
-        res.render('index', {"tasks": tasks});
-      });
+    .then(function (tasks) {
+      res.render('index', {"tasks": tasks});
+    });
   },
   indexApi: function (req, res) {
     Task.findAll()
-      .then(function (tasks) {
-        res.json(tasks);
-      });
+    .then(function (tasks) {
+      res.json(tasks);
+    });
   },
-};
-module.exports = taskController;
+  user: function (req, res) {
+    if(req.body.action == "create"){
+      User.findOrCreate({ where: { username: req.body.username }, defaults: { password: req.body.password } })
+      .spread((user, created) => {
+        console.log(user.get({
+          plain: true
+        }))
+        console.log(created)
+      })
+
+      return res.redirect('/tasks');
+    }
+
+    if(req.body.action == "create"){
+    }
+
+ },
+  login: function(req, res) {
+      res.render('login');
+  }
+  };
+  module.exports = taskController;
--- server.js
+++ server.js
@@ -7,21 +7,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine','ejs');
-var todoList =["wash the car and change oil",
-              "buy groceries and make dinner"]
-
-var routes = require('./routes')(app);
-//===========Express Routes Here=======//
-app.get("/",function(req,res){
-  res.render("index.ejs")
-});
-//catch all other routes
-app.get("*",function(req,res){
-  res.send("<h1>Invalid Pages</h1>")
-});
-
-//=====sever listening on port3000
 app.listen(3000, function() {
   db.sequelize.sync();
-  console.log("sever started on port 3000");
 });
+
+var routes = require('./routes')(app);
